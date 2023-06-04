8fce546d1bef3583e8f0c37039a445e2f1c27a58
Ivan <zakharovivann@gmail.com>
2023-06-04 12:32:37 +0300

    Add submodule

---
+++ src/SystemsOfLinearEquationsSolve/SLAEGauss.cc
@@ -0,0 +1,8 @@
+//
+// Created by Иван Захаров on 04.06.2023.
+//
+
+#include "SLAEGauss.h"
+
+namespace s21 {
+} // s21
---
+++ src/SystemsOfLinearEquationsSolve/SLAEGauss.h
@@ -0,0 +1,16 @@
+//
+// Created by Иван Захаров on 04.06.2023.
+//
+
+#ifndef PARALLELS_SLAEGAUSS_H
+#define PARALLELS_SLAEGAUSS_H
+
+namespace s21 {
+
+class SLAEGauss {
+
+};
+
+} // s21
+
+#endif //PARALLELS_SLAEGAUSS_H
